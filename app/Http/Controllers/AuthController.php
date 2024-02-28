c544fc1b344d73c37d70208ed62e2f432b3d9f78
NicoAriestP <nicoariestputra@gmail.com>
2024-02-28 18:10:53 +0700

    feat:auth using JWT

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -67,7 +67,7 @@ class AuthController extends Controller
 
         if ($user) {
             return response()->json([
-                'user' => $user,
+                'data' => $user,
             ], 200);
         }
     }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,12 +17,20 @@ use App\Http\Controllers\AuthController;
 */
 
 Route::controller(AuthController::class)->group(function () {
-    Route::post('/auth/register', 'register')->name('api.auth.register');
-    Route::post('/auth/login', 'login')->name('api.auth.login');
+    Route::post('/register', 'register')->name('api.auth.register');
+    Route::post('/login', 'login')->name('api.auth.login');
 });
 
 Route::middleware('auth:api')->controller(AuthController::class)->group(function() {
-    Route::get('/auth/profile', 'profile')->name('api.auth.profile');
-    Route::post('/auth/logout', 'logout')->name('api.auth.logout');
-    Route::get('/auth/refresh', 'refresh')->name('api.auth.refresh');
+    Route::get('/profile', 'profile')->name('api.auth.profile');
+    Route::post('/logout', 'logout')->name('api.auth.logout');
+    Route::get('/refresh', 'refresh')->name('api.auth.refresh');
+});
+
+Route::middleware('auth:api')->controller(UserController::class)->group(function () {
+    Route::get('/users', 'index')->name('api.user.list');
+    Route::get('/users/{model}', 'detail')->name('api.user.detail');
+    Route::post('/users', 'create')->name('api.user.create');
+    Route::put('/users/{model}', 'update')->name('api.user.update');
+    Route::delete('/users/{model}', 'destroy')->name('api.user.delete');
 });
