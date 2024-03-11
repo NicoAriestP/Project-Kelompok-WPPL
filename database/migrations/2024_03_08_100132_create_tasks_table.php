9d165477fdb59751e839dc654da5e926e9ae91ef
NicoAriestP <nicoariestputra@gmail.com>
2024-03-12 04:51:25 +0700

    sorting migration tasks & comments

---
+++ database/migrations/2024_03_08_100132_create_tasks_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('category_id')
+                ->nullable()
+                ->constrained('categories')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+
+            $table->string('name', 255);
+            $table->text('description');
+            $table->date('due_at');
+            $table->string('priority', 20);
+            $table->string('effort', 20);
+            $table->string('status', 20);
+            $table->string('file', 2048)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
