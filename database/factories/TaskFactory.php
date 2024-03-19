7f485eb27f4935d87c70867f201e7bfeadda1b2a
NicoAriestP <nicoariestputra@gmail.com>
2024-03-19 23:50:25 +0700

    fix:TaskFactory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use App\Models\Category;
+use App\Models\User;
 use App\Enum\Task\PriorityType;
 use App\Enum\Task\EffortType;
 use App\Enum\Task\StatusType;
